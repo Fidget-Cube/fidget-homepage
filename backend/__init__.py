0ab824f88f1bac7ce6d52be053881c8414fd0663
Fidget-Cube <hobbes399@gmail.com>
2023-02-22 16:49:31 -0800

    Host with Nginx

--- backend/__init__.py
+++
@@ -1,9 +0,0 @@
-import os, random
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-
-    app.config.from_pyfile('config.py', silent=True)
-
-    return app
